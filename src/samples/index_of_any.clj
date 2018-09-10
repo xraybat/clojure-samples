35136ddbef40d78dbdd362f7ef665a95c96e3233
xraybat <xraybat100@gmail.com>
2018-09-10 17:20:40 +1000

    index-of-any

--- src/samples/index_of_any.clj
+++ src/samples/index_of_any.clj
@@ -1,1 +1,20 @@
 (ns samples.index-of-any)
+
+(defn indexed [coll]
+  (map-indexed vector coll))
+
+(println (indexed "abcde"))
+
+(defn index-filter [pred coll]
+  (when pred
+    (for [[idx elt] (indexed coll) :when (pred elt)]
+      idx)))
+
+(println (index-filter #{\a \b} "abcdbbb"))
+(println (index-filter #{\a \b} "xyz"))
+
+(defn index-of-any [pred coll]
+  (first (index-filter pred coll)))
+
+(println (index-of-any #{\z \a} "zzabyycdxx"))
+(println (index-of-any #{\b \y} "zzabyycdxx"))
