ea94737fc78ac7780541c1aa3658866ab8ce2bf3
xraybat <xraybat100@gmail.com>
2018-09-11 12:19:10 +1000

    index-of-any

--- src/samples/index_of_any.clj
+++ src/samples/index_of_any.clj
@@ -18,3 +18,26 @@
 
 (println (index-of-any #{\z \a} "zzabyycdxx"))
 (println (index-of-any #{\b \y} "zzabyycdxx"))
+
+;; also works with other seqs
+;; vectors
+(println (index-of-any #{\z \a} [\z \z \a \b \y \y \c \d \x \x]))
+(println (index-of-any #{\b \y} [\z \z \a \b \y \y \c \d \x \x]))
+
+(println (index-of-any #{:z :a} [:z :z :a :b :y :y :c :d :x :x]))
+(println (index-of-any #{:b :y} [:z :z :a :b :y :y :c :d :x :x]))
+
+;; lists
+(println (index-of-any #{\z \a} '(\z \z \a \b \y \y \c \d \x \x)))
+(println (index-of-any #{\b \y} '(\z \z \a \b \y \y \c \d \x \x)))
+
+;; sets (but no dupes allowed and index order might be sketchy)
+(println (index-of-any #{\z \a} #{\z \a \b \y \c \d \x}))
+(println (index-of-any #{\b \y} #{\z \a \b \y \c \d \x}))
+
+;; more general usages
+(println)
+(println (nth
+           (index-filter #{:h} [:t :t :h :t :h :t :t :t :h :h])
+           2))                                              ;; 3rd occurrence, nth-1, starts at 0
+
